bfb360e1d4b4b0dab0faed14bde8f0093c51803b
Michael de Bruin <git@buum.nl>
2012-11-27 15:56:57 +0100

    Request based on last tweet id in our cache, making the request more efficient

--- twit2prowl.pl
+++ twit2prowl.pl
@@ -37,35 +37,33 @@ else {
   close(TIMESTAMP);
 }
 
-my $mentions = $tweets->mentions();
+my $mentions = $tweets->mentions({ since_id => $lasttweet });
 foreach my $mention (@{$mentions}) {
-  if ($mention->{id} > $lasttweet) {
-    # Generate our HTTP request
-    $userAgent = LWP::UserAgent->new;
-    $userAgent->agent("ProwlScript/1.2");
-    $userAgent->env_proxy();
+  # Generate our HTTP request
+  $userAgent = LWP::UserAgent->new;
+  $userAgent->agent("ProwlScript/1.2");
+  $userAgent->env_proxy();
 
-    # URL encode our tweet
-    $mention->{text} =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg; 
-    $user = $mention->{user}->{screen_name} . " (" . $mention->{user}->{name} . ")";
-    $user =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
-    print "Sending tweet with id " . $mention->{id} . "\n" if $cfg::debug;
-    $requestURL = sprintf("https://prowlapp.com/publicapi/add?apikey=%s&application=twit2prowl&event=%s&description=%s&priority=%d",
-                           $cfg::apikey,
-                           $user,
-                           $mention->{text},
-                           $cfg::priority);
-    $request = HTTP::Request->new(GET => $requestURL);
-    $response = $userAgent->request($request);
-    if ($response->is_success) {
-      print "Notification successfully posted.\n" if $cfg::debug;
-    } elsif ($response->code == 401) {
-      print STDERR "Notification not posted: incorrect API key.\n";
-    } else {
-      print STDERR "Notification not posted: " . $response->content . "\n";
-    }
-    if ( $mention->{id} > $cache) { $cache = $mention->{id}; }
+  # URL encode our tweet
+  $mention->{text} =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg; 
+  $user = $mention->{user}->{screen_name} . " (" . $mention->{user}->{name} . ")";
+  $user =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
+  print "Sending tweet with id " . $mention->{id} . "\n" if $cfg::debug;
+  $requestURL = sprintf("https://prowlapp.com/publicapi/add?apikey=%s&application=twit2prowl&event=%s&description=%s&priority=%d",
+                        $cfg::apikey,
+                        $user,
+                        $mention->{text},
+                        $cfg::priority);
+  $request = HTTP::Request->new(GET => $requestURL);
+  $response = $userAgent->request($request);
+  if ($response->is_success) {
+    print "Notification successfully posted.\n" if $cfg::debug;
+  } elsif ($response->code == 401) {
+    print STDERR "Notification not posted: incorrect API key.\n";
+  } else {
+    print STDERR "Notification not posted: " . $response->content . "\n";
   }
+  if ( $mention->{id} > $cache) { $cache = $mention->{id}; }
 }
 
 $cache = $lasttweet unless $cache;
